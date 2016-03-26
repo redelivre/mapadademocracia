a2e66f08062f1f551ec0c47b8e6a07c19a926e1f
Niv Sardi <xaiki@evilgiggle.com>
2016-03-26 16:27:47 -0300

    port python changes to js
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- fixture/get-fixture.js
+++ fixture/get-fixture.js
@@ -3,5 +3,9 @@ var googleDocsCms = require('google-docs-cms');
 var id = '1H_cnrYN6GjkTNynyL4PpJLMdIP-kaLrrOdtyukrwcAs';
 
 googleDocsCms({id: id}).then(function(res) {
-    console.log(JSON.stringify(res));
+    var deputados = res['Todos os Deputados'].map (function (dep){
+        dep.politico_comissao = (dep.politico_comissao === 'sim') ? true : false;
+        return dep;
+    })
+    console.log(JSON.stringify(deputados));
 })
