c67c5937d74e9404ea44b3c4f60a5c3643a13d8b
Luis Fagundes <lhfagundes@hacklab.com.br>
2016-04-01 13:46:46 -0300

    voltando commit do rafa. e aproveito pra atualizar o index.html de desenvolvimento

--- dist/home.js
+++ dist/home.js
@@ -104,8 +104,14 @@ $(document).ready(function() {
             }
             return array;
         }
-        var addresses = comissao.map(function(e){ return e.politico_email; });
-        var _addresses = shuffleArray(deputados.map(function(e){ return e.politico_email; }));
+
+        function mapEmail(e){
+            return e.politico_email;
+        }
+
+
+        var addresses = comissao.map(mapEmail);
+        var _addresses = shuffleArray(deputados.map(mapEmail));
 
         _addresses.forEach(function(e){
             if(addresses.indexOf(e) < 0 && addresses.length < 100){
@@ -116,17 +122,19 @@ $(document).ready(function() {
 
         var bcc = "mapadademocracia@culturalivre.org";
 
+        
         addresses = addresses.join(',');
 
         var element = $('#home-acao-' + grupo + ' span.message-data')
         var subject;
         var body;
 
+        console.log('aqui')
         if (element.length > 0) {
             var subjects = element.find('subject');
             var index = Math.floor(Math.random() * (subjects.length));
             subject = $(subjects[index]).text();
-            console.log(index)
+            
             body = element.find('msg-body').text().replace(/\n/g, '%0D%0A%0D%0A\n');
             element.hide();
         } else {
@@ -143,7 +151,119 @@ $(document).ready(function() {
 
         var href_mailto="mailto:" + addresses + "?bcc=" + bcc + '&subject=' + subject + '&body=' + body;
 
-        $('#home-acao-' + grupo + ' a.et_pb_promo_button.et_pb_button').attr('href', href_mailto);
+        
+        if(isMobile.any){
+            return;
+        }
+
+
+
+        var urls = {
+          'gmail'   : 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&shva=1' + 
+                      '&to='      + addresses +
+                      '&bcc='     + bcc +         
+                      '&su='      + subject +
+                      '&body='    + body,
+          'yahoo'   : 'https://compose.mail.yahoo.com/?' +
+                      'to='       + comissao.map(mapEmail).join(',') +
+                      '&bcc='     + bcc +
+                      '&subject='    + subject +
+                      '&body='    + body,
+          'hotmail' : 'https://mail.live.com/#viewmodel=IMailComposeViewModelFactory' +
+                      '&to='      + comissao.slice(0,1).map(mapEmail).join(',')
+                      // '&cco='     + bcc +
+                      // '&bcc='     + bcc +
+                      // '&subject=' + subject +
+                      // '&body='    + body
+        }
+
+        $addresses = $('<div id="emails-' + grupo + '">')
+            .css({position:'absolute', display:'none', width:'1px', height:'1px', overflow:'hidden'})
+            .text(deputados.map(mapEmail).join(','));
+
+        $body = $('<div id="corpo-' + grupo + '">')
+            .css({position:'absolute', display:'none', width:'1px', height:'1px', overflow:'hidden'})
+            .text(decodeURIComponent(body));
+
+        $(document.body).append($addresses);
+        $(document.body).append($body);
+
+        var $button = $('#home-acao-' + grupo + ' a.et_pb_promo_button.et_pb_button')
+            .attr('href', href_mailto);
+
+
+
+        var $div = $('<div>')
+            .css({textAlign: 'center', marginTop: '10px', marginLeft: '-25px', marginRight: '-25px'});
+
+        var $a = $('<a>')
+            .css({textDecoration: 'underline', color:'white'})
+            .text('Não funcionou?')
+            .click(function(){
+                $box.slideDown('fast');
+            })
+            .appendTo($div);
+
+        var $box = $('<div>Tente uma das alternativas abaixo:<br></div>')
+            .css({background:'rgba(0,0,0,.2)', display: 'none', padding: '10px', marginTop: '15px', fontSize: '13px'})
+            .appendTo($div);
+
+        var webmailCss = {color:'white', margin: '5px', fontSize:'13px', marginTop: '15px', display:'inline-block'};
+
+
+        var $gmail = $('<a class="webmail-button et_pb_button"></a>')
+            .data('url', urls.gmail)
+            .css(webmailCss)
+            .text('Gmail')
+            .appendTo($box);
+
+        // var $hotmail = $('<a class="webmail-button et_pb_button"></a>')
+        //     .data('url', urls.hotmail)
+        //     .css(webmailCss)
+        //     .text('Hotmail')
+        //     .appendTo($box);
+
+        // var $yahoo = $('<a class="webmail-button et_pb_button"></a>')
+        //     .data('url', urls.yahoo)
+        //     .css(webmailCss)
+        //     .text('Yahoo')
+        //     .appendTo($box);
+
+        [$gmail/*, $yahoo, $hotmail */].forEach(function($e){
+            $e.click(function(){
+                var newWindow = window.open($(this).data('url'), '_blank', 'location=0,statusbar=0,menubar=0,width=600,height=600');
+            });
+        });
+
+        $('<p>')
+            .text('Se você não usa o Gmail, você pode clicar nos botões abaixo para copiar os endereços de email dos deputados e o corpo do email para a área de transferência.')
+            .appendTo($box)
+
+        var $clipboard_emails = $('<a class="et_pb_button"></a>')
+            .css(webmailCss).css('display', 'block')
+            .text('Copiar todos os emails')
+            .appendTo($box)
+            .click(function(){
+                copyToClipboard(document.getElementById('emails-' + grupo));
+                $(this).focus();
+                return false;
+            });
+
+        var $clipboard_emails = $('<a class="et_pb_button"></a>')
+            .css(webmailCss).css('display', 'block')
+            .text('Copiar texto do email')
+            .appendTo($box)
+            .click(function(){
+                copyToClipboard(document.getElementById('corpo-' + grupo));
+                $(this).focus();
+                return false;
+            });
+
+
+
+        $div.find('a').css('cursor', 'pointer');
+
+        $button.after($div);
     };
 
 
@@ -208,4 +328,196 @@ $(document).ready(function() {
         $('.bloco-partido').hide();
     }
 
-})
+});
+
+
+
+
+/**
+ * isMobile.js v0.4.0
+ *
+ * A simple library to detect Apple phones and tablets,
+ * Android phones and tablets, other mobile devices (like blackberry, mini-opera and windows phone),
+ * and any kind of seven inch device, via user agent sniffing.
+ *
+ * @author: Kai Mallea (kmallea@gmail.com)
+ *
+ * @license: http://creativecommons.org/publicdomain/zero/1.0/
+ */
+(function (global) {
+
+    var apple_phone         = /iPhone/i,
+        apple_ipod          = /iPod/i,
+        apple_tablet        = /iPad/i,
+        android_phone       = /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i, // Match 'Android' AND 'Mobile'
+        android_tablet      = /Android/i,
+        amazon_phone        = /(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,
+        amazon_tablet       = /(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,
+        windows_phone       = /IEMobile/i,
+        windows_tablet      = /(?=.*\bWindows\b)(?=.*\bARM\b)/i, // Match 'Windows' AND 'ARM'
+        other_blackberry    = /BlackBerry/i,
+        other_blackberry_10 = /BB10/i,
+        other_opera         = /Opera Mini/i,
+        other_chrome        = /(CriOS|Chrome)(?=.*\bMobile\b)/i,
+        other_firefox       = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i, // Match 'Firefox' AND 'Mobile'
+        seven_inch = new RegExp(
+            '(?:' +         // Non-capturing group
+
+            'Nexus 7' +     // Nexus 7
+
+            '|' +           // OR
+
+            'BNTV250' +     // B&N Nook Tablet 7 inch
+
+            '|' +           // OR
+
+            'Kindle Fire' + // Kindle Fire
+
+            '|' +           // OR
+
+            'Silk' +        // Kindle Fire, Silk Accelerated
+
+            '|' +           // OR
+
+            'GT-P1000' +    // Galaxy Tab 7 inch
+
+            ')',            // End non-capturing group
+
+            'i');           // Case-insensitive matching
+
+    var match = function(regex, userAgent) {
+        return regex.test(userAgent);
+    };
+
+    var IsMobileClass = function(userAgent) {
+        var ua = userAgent || navigator.userAgent;
+
+        // Facebook mobile app's integrated browser adds a bunch of strings that
+        // match everything. Strip it out if it exists.
+        var tmp = ua.split('[FBAN');
+        if (typeof tmp[1] !== 'undefined') {
+            ua = tmp[0];
+        }
+
+        // Twitter mobile app's integrated browser on iPad adds a "Twitter for
+        // iPhone" string. Same probable happens on other tablet platforms.
+        // This will confuse detection so strip it out if it exists.
+        tmp = ua.split('Twitter');
+        if (typeof tmp[1] !== 'undefined') {
+            ua = tmp[0];
+        }
+
+        this.apple = {
+            phone:  match(apple_phone, ua),
+            ipod:   match(apple_ipod, ua),
+            tablet: !match(apple_phone, ua) && match(apple_tablet, ua),
+            device: match(apple_phone, ua) || match(apple_ipod, ua) || match(apple_tablet, ua)
+        };
+        this.amazon = {
+            phone:  match(amazon_phone, ua),
+            tablet: !match(amazon_phone, ua) && match(amazon_tablet, ua),
+            device: match(amazon_phone, ua) || match(amazon_tablet, ua)
+        };
+        this.android = {
+            phone:  match(amazon_phone, ua) || match(android_phone, ua),
+            tablet: !match(amazon_phone, ua) && !match(android_phone, ua) && (match(amazon_tablet, ua) || match(android_tablet, ua)),
+            device: match(amazon_phone, ua) || match(amazon_tablet, ua) || match(android_phone, ua) || match(android_tablet, ua)
+        };
+        this.windows = {
+            phone:  match(windows_phone, ua),
+            tablet: match(windows_tablet, ua),
+            device: match(windows_phone, ua) || match(windows_tablet, ua)
+        };
+        this.other = {
+            blackberry:   match(other_blackberry, ua),
+            blackberry10: match(other_blackberry_10, ua),
+            opera:        match(other_opera, ua),
+            firefox:      match(other_firefox, ua),
+            chrome:       match(other_chrome, ua),
+            device:       match(other_blackberry, ua) || match(other_blackberry_10, ua) || match(other_opera, ua) || match(other_firefox, ua) || match(other_chrome, ua)
+        };
+        this.seven_inch = match(seven_inch, ua);
+        this.any = this.apple.device || this.android.device || this.windows.device || this.other.device || this.seven_inch;
+
+        // excludes 'other' devices and ipods, targeting touchscreen phones
+        this.phone = this.apple.phone || this.android.phone || this.windows.phone;
+
+        // excludes 7 inch devices, classifying as phone or tablet is left to the user
+        this.tablet = this.apple.tablet || this.android.tablet || this.windows.tablet;
+
+        if (typeof window === 'undefined') {
+            return this;
+        }
+    };
+
+    var instantiate = function() {
+        var IM = new IsMobileClass();
+        IM.Class = IsMobileClass;
+        return IM;
+    };
+
+    if (typeof module !== 'undefined' && module.exports && typeof window === 'undefined') {
+        //node
+        module.exports = IsMobileClass;
+    } else if (typeof module !== 'undefined' && module.exports && typeof window !== 'undefined') {
+        //browserify
+        module.exports = instantiate();
+    } else if (typeof define === 'function' && define.amd) {
+        //AMD
+        define('isMobile', [], global.isMobile = instantiate());
+    } else {
+        global.isMobile = instantiate();
+    }
+
+})(this);
+
+
+function copyToClipboard(elem) {
+      // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        // must use a temporary form element for the selection and copy
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.textContent;
+    }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    
+    // copy the selection
+    var succeed;
+    try {
+          succeed = document.execCommand("copy");
+    } catch(e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+    
+    if (isInput) {
+        // restore prior selection
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        // clear temporary content
+        target.textContent = "";
+    }
+    return succeed;
+}
